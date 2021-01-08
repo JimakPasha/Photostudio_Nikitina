4b42586603c2f4af1a40611fe287ec19edff916d
JimakPasha <Jimak@tut.by>
2021-01-09 02:56:47 +0300

    added header and about sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-  
+
+  $('.love-story__slider').slick({
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>'
+  });
+
 });
