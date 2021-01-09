d7eab5dba769eb02885d30ce0231aca55780395a
JimakPasha <Jimak@tut.by>
2021-01-09 15:58:59 +0300

    added portfolio and studio sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,6 @@
 $(function () {
 
-  $('.love-story__slider').slick({
-    // autoplay: true,
-    // autoplaySpeed: 2000,
+  $('.portfolio__item-slider-images').slick({
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>'
   });
