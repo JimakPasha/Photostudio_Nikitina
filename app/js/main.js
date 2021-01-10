89dfd31550be74703339953e56dfd0c2b557c319
JimakPasha <Jimak@tut.by>
2021-01-10 11:44:21 +0300

    added contacts footer sections and scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 $(function () {
 
+  $(".menu a, .footer__links-list a").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 800);
+	});
+
   $('.portfolio__item-slider-images').slick({
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>'
